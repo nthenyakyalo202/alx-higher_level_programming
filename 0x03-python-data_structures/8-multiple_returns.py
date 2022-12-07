a6f46bbb265b04a574bbf3be00ede56ef4e0459c
nthenyakyalo202 <nthenyakyalo202@gmail.com>
2022-12-07 01:21:05 +0000

    more return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-    my_tuple = ()
-    if len(sentence) ==0:
-       my_tple =0, 'None"
-else:
-   my_tuple = len(sentence), sentence[0]
-return my_tuple
+    if sentence != '':
+        first_char = sentence[0]
+    else:
+        first_char = None
+    return (len(sentence), first_char)
